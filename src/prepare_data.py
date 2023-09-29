63a7ccae7b4bf332c2ed8fa2707ba9c8101ec6f5
max_lutz <max.lutz.09@gmail.com>
2023-09-29 11:15:14 -0600

    create file to preprocess data

---
+++ src/prepare_data.py
@@ -0,0 +1,23 @@
+import os
+import shutil
+
+import urllib.request
+from loguru import logger
+
+
+def download_file(directory, filename):
+    url = f"https://github.com/khanhnamle1994/instacart-orders/raw/master/data/{filename}?download="
+    logger.debug(f"Downloading {filename}...")
+    urllib.request.urlretrieve(url, os.path.join(directory, filename))
+
+
+if __name__ == "__main__":
+    files = ["order_products__prior.csv", "orders.csv", "products.csv", "aisles.csv", "departments.csv"]
+    logger.debug(f"Downloading {len(files)} files")
+
+    shutil.rmtree('../data', ignore_errors=True)
+    os.mkdir("../data")
+    os.mkdir("../data/raw")
+
+    for file in files:
+        download_file(directory="../data/raw", filename=file)
