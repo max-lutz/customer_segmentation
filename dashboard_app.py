10d273760dcbfe0b76a8708c95b6e4f69e64ea19
max_lutz <max.lutz.09@gmail.com>
2023-09-30 12:30:40 -0600

    create plotly indicators

--- dashboard_app.py
+++ dashboard_app.py
@@ -34,6 +34,30 @@ def hide_streamlit_header_footer():
     st.markdown(hide_st_style, unsafe_allow_html=True)
 
 
+def plot_metric(number, delta_number, title):
+    config = {'staticPlot': True}
+
+    fig = go.Figure()
+    fig.add_trace(go.Indicator(
+        mode="number+delta",
+        value=number,
+        title={"text": title, "font": {"size": 24}},
+        delta={'reference': delta_number, 'relative': True},
+        domain={'row': 0, 'column': 0}))
+
+    fig.update_xaxes(visible=False, fixedrange=True)
+    fig.update_yaxes(visible=False, fixedrange=True)
+    fig.update_layout(
+        # paper_bgcolor="lightgrey",
+        margin=dict(t=30, b=0),
+        showlegend=False,
+        plot_bgcolor="white",
+        height=100,
+    )
+
+    st.plotly_chart(fig, use_container_width=True, config=config)
+
+
 def main():
 
     hide_streamlit_header_footer()
@@ -49,7 +73,7 @@ def main():
             week_list = [file for file in os.listdir("data/processed") if file[0:6] == "orders"]
             week_list = [file[7:-4] for file in week_list]
             week_list.sort(reverse=True)
-            week = st.selectbox('Week', week_list[0:-1])
+            week = st.selectbox('Week', week_list[1:-1])
 
             df_orders = load_data(f"data/processed/orders_{week}.zip")
             df_orders_prev = load_data(f"data/processed/orders_{week_list[week_list.index(week)+1]}.zip")
@@ -68,16 +92,18 @@ def main():
 
     col_1, _, col_2 = st.columns((ROW, .05, ROW))
     with col_1:
-        _, col_1_1, _, col_1_2, _ = st.columns((0.1, ROW, .01, ROW, 0.1))
+        _, col_1_1, _, col_1_2, _, col_1_3, _ = st.columns((0.1, ROW, .01, ROW, 0.1, ROW, 0.1))
         with col_1_1:
-            st.markdown("### **Total clients**")
-            st.markdown("# **416**")
-            st.markdown(":red[↓7%]")
+            plot_metric(len(df_orders["user_id"].unique()), len(df_orders_prev["user_id"].unique()), "Total users")
 
-            st.write("Take inspiration from https://github.com/andfanilo/social-media-tutorials/blob/master/20230816-stdashboard/streamlit_app.py")
-            st.write("and https://www.klipfolio.com/resources/dashboard-examples/sales")
+            # st.write("Take inspiration from https://github.com/andfanilo/social-media-tutorials/blob/master/20230816-stdashboard/streamlit_app.py")
+            # st.write("and https://www.klipfolio.com/resources/dashboard-examples/sales")
         with col_1_2:
-            st.write("Total orders")
+            plot_metric(len(df_orders["order_id"].unique()), len(df_orders_prev["order_id"].unique()), "Total orders")
+
+        with col_1_3:
+            plot_metric(df_orders["basket_size"].mean(), df_orders_prev["basket_size"].mean(), "Average basket size")
+
     st.write()
 
     st.write(df_orders)
