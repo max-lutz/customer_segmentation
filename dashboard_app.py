ad1a60df9037fd99d2a3437f3064765d7be88432
max_lutz <max.lutz.09@gmail.com>
2023-10-02 09:42:19 -0600

    WIP: load volumes and new users df

--- dashboard_app.py
+++ dashboard_app.py
@@ -11,6 +11,7 @@ import streamlit as st
 
 import plotly.graph_objects as go
 import plotly.express as px
+from plotly.subplots import make_subplots
 
 
 # configuration of the page
@@ -86,6 +87,12 @@ def main():
     df_aisles = load_data(f"data/raw/aisles.csv")
     df_departments = load_data(f"data/raw/departments.csv")
 
+    df_new_users = load_data("data/processed/new_users.zip")
+    df_new_users["date"] = pd.to_datetime(df_new_users["date"])
+
+    df_volumes = load_data("data/processed/volumes.zip")
+    df_volumes["date"] = pd.to_datetime(df_volumes["date"])
+
     with header_button:
         button_1, button_2, button_3, button_4 = st.columns(4)
         with button_1:
@@ -97,14 +104,14 @@ def main():
             df_orders = load_data(f"data/processed/orders_{week}.zip")
             df_orders_prev = load_data(f"data/processed/orders_{week_list[week_list.index(week)+1]}.zip")
 
-        with button_2:
-            report = st.selectbox('Report', ["Sales", "Customer"])
+        # with button_2:
+        #     report = st.selectbox('Report', ["Sales", "Customer"])
 
-        with button_3:
-            department = st.selectbox('Department', ["All", "Department 1"])
+        # with button_3:
+        #     department = st.selectbox('Department', ["All", "Department 1"])
 
-        with button_4:
-            ailse = st.selectbox('Aisle', ["All", "Aisle 1"])
+        # with button_4:
+        #     ailse = st.selectbox('Aisle', ["All", "Aisle 1"])
 
     st.write("")
     st.write("")
@@ -112,23 +119,48 @@ def main():
     week_occurrences = get_occurrences(df_orders, df_products, df_aisles, df_departments)
     week_occurrences_prev = get_occurrences(df_orders_prev, df_products, df_aisles, df_departments)
 
-    row_1_col_1, _, row_1_col_2 = st.columns((ROW, .05, ROW))
+    row_1_col_1, row_1_col_2, row_1_col_3, row_1_col_4, row_1_col_5 = st.columns(5)
     with row_1_col_1:
-        _, row_1_col_1_1, _, row_1_col_1_2, _, row_1_col_1_3, _ = st.columns((0.1, ROW, .01, ROW, 0.1, ROW, 0.1))
-        with row_1_col_1_1:
-            plot_metric(len(df_orders["user_id"].unique()), len(df_orders_prev["user_id"].unique()), "Total users")
+        plot_metric(len(df_orders["user_id"].unique()), len(df_orders_prev["user_id"].unique()), "Total users")
 
-            # st.write("Take inspiration from https://github.com/andfanilo/social-media-tutorials/blob/master/20230816-stdashboard/streamlit_app.py")
-            # st.write("and https://www.klipfolio.com/resources/dashboard-examples/sales")
-        with row_1_col_1_2:
-            plot_metric(len(df_orders["order_id"].unique()), len(df_orders_prev["order_id"].unique()), "Total orders")
+        # st.write("Take inspiration from https://github.com/andfanilo/social-media-tutorials/blob/master/20230816-stdashboard/streamlit_app.py")
+        # st.write("and https://www.klipfolio.com/resources/dashboard-examples/sales")
+    with row_1_col_2:
+        plot_metric(len(df_orders["order_id"].unique()), len(df_orders_prev["order_id"].unique()), "Total orders")
 
-        with row_1_col_1_3:
-            plot_metric(df_orders["basket_size"].mean(), df_orders_prev["basket_size"].mean(), "Average basket size")
+    with row_1_col_3:
+        plot_metric(df_orders["basket_size"].mean(), df_orders_prev["basket_size"].mean(), "Average basket size")
+
+    with row_1_col_4:
+        plot_metric(df_orders["days_since_prior_order"].mean(),
+                    df_orders_prev["days_since_prior_order"].mean(), "Days between orders")
+
+    with row_1_col_5:
+        week_end, week_start = pd.to_datetime(week.split("_")[0]), pd.to_datetime(week.split("_")[1])
+        df_new_users_this_week = df_new_users[(df_new_users["date"] >= week_start) & (df_new_users["date"] <= week_end)]
+
+        prev_week = week_list[week_list.index(week)+1]
+        week_end, week_start = pd.to_datetime(prev_week.split("_")[0]), pd.to_datetime(prev_week.split("_")[1])
+        df_new_users_last_week = df_new_users[(df_new_users["date"] >= week_start) & (df_new_users["date"] <= week_end)]
+        plot_metric(len(df_new_users_this_week), len(df_new_users_last_week), "New users")
 
     st.write()
-    row_2_col_1, _, row_2_col_2,  _, row_2_col_3 = st.columns((ROW, .05, ROW, .05, ROW))
+    row_2_col_1, _, row_2_col_2, _, row_2_col_3 = st.columns((ROW*1.5, .05, ROW, .05, ROW))
     with row_2_col_1:
+        df_volumes = df_volumes[df_volumes["date"] <= pd.to_datetime(week.split("_")[0])]
+        df_wv = df_volumes.groupby(pd.Grouper(key='date', freq='W')).sum().reset_index()
+
+        fig = make_subplots(specs=[[{"secondary_y": True}]])
+        fig.add_trace(go.Bar(x=df_wv['date'], y=df_wv['order_number'], name="Orders"), secondary_y=False)
+        fig.add_trace(go.Scatter(x=df_wv['date'], y=df_wv['basket_size'], name="Sold items"), secondary_y=True)
+
+        fig.update_layout(title_text="Weekly sales")
+        fig.update_xaxes(title_text="week")
+        fig.update_yaxes(title_text="Order volume", secondary_y=False)
+        fig.update_yaxes(title_text="Items volume", secondary_y=True)
+        st.plotly_chart(fig, use_container_width=True, config={'displayModeBar': False})
+
+    with row_2_col_2:
         df_ = df_orders.groupby("date").order_number.count().reset_index()
         df_["date"] = pd.to_datetime(df_["date"]).dt.day_name()
 
@@ -143,39 +175,22 @@ def main():
         fig = go.Figure(data=data, layout=go.Layout(title='Average daily orders'))
         st.plotly_chart(fig, use_container_width=True, config={'displayModeBar': False})
 
-    with row_2_col_2:
-        df_top_deps = pd.DataFrame(week_occurrences.groupby("department")["count"].sum()).reset_index()
-        df_top_deps = df_top_deps.sort_values("count", ascending=False)[0:5]
-
-        df_top_deps_prev = pd.DataFrame(week_occurrences_prev.groupby("department")["count"].sum()).reset_index()
-        df_top_deps = df_top_deps.merge(df_top_deps_prev, how="left", on="department", suffixes=["", "_prev"])
-
-        data = [
-            go.Bar(x=df_top_deps['department'], y=df_top_deps['count'], name="This week"),
-            go.Scatter(x=df_top_deps['department'], y=df_top_deps['count_prev'], name="Previous week")
-        ]
-
-        fig = go.Figure(data=data, layout=go.Layout(title='Top five departments'))
-        st.plotly_chart(fig, use_container_width=True, config={'displayModeBar': False})
-
-    st.write(week_occurrences)
     with row_2_col_3:
-        df_top_deps = pd.DataFrame(week_occurrences.groupby("product_name")["count"].sum()).reset_index()
-        df_top_deps = df_top_deps.sort_values("count", ascending=False)[0:5]
+        df_ = df_orders.groupby("datetime").order_number.count().reset_index()
+        df_["time"] = pd.to_datetime(df_["datetime"]).dt.time
+        df_["time_of_day"]
+        st.write(df_)
 
-        df_top_deps_prev = pd.DataFrame(week_occurrences_prev.groupby("product_name")["count"].sum()).reset_index()
-        df_top_deps = df_top_deps.merge(df_top_deps_prev, how="left", on="product_name", suffixes=["", "_prev"])
+        df_prev_ = df_orders_prev.groupby("datetime").order_number.count().reset_index()
+        df_prev_["time"] = pd.to_datetime(df_prev_["datetime"]).dt.time
 
         data = [
-            go.Bar(x=df_top_deps['product_name'], y=df_top_deps['count'], name="This week"),
-            go.Scatter(x=df_top_deps['product_name'], y=df_top_deps['count_prev'], name="Previous week")
+            go.Bar(x=df_['time'], y=df_['order_number'], name="This week"),
+            go.Scatter(x=df_prev_['time'], y=df_prev_['order_number'], name="Previous week")
         ]
 
-        fig = go.Figure(data=data, layout=go.Layout(title='Top five products'))
+        fig = go.Figure(data=data, layout=go.Layout(title='time of order'))
         st.plotly_chart(fig, use_container_width=True, config={'displayModeBar': False})
-
-    st.write(df_orders)
-    st.write(df_orders_prev)
 
 
 if __name__ == "__main__":
